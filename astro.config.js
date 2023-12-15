870dc7e8c5358645cbd72da9cdd74cbb9bf05986
ShellWen <me@shellwen.com>
2023-12-16 04:35:29 +0800

    :sparkles: add Sentry

--- astro.config.js
+++ astro.config.js
@@ -3,13 +3,27 @@ import mdx from '@astrojs/mdx'
 import react from '@astrojs/react'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
+import sentry from '@sentry/astro'
 import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://shellwen.com',
   output: 'hybrid',
-  integrations: [mdx(), react(), sitemap(), tailwind()],
+  integrations: [
+    mdx(),
+    react(),
+    sitemap(),
+    tailwind(),
+    sentry({
+      dsn: 'https://7df36d7c0ed618e59ae41c6ded5b9a7c@o4506401530511360.ingest.sentry.io/4506401532018688',
+      release: `shellwens-things@${process.env.npm_package_version ?? 'unknown'}`,
+      sourceMapsUploadOptions: {
+        project: 'shellwens-things',
+        authToken: process.env.SENTRY_AUTH_TOKEN,
+      },
+    }),
+  ],
   adapter: cloudflare({
     imageService: 'cloudflare',
   }),
