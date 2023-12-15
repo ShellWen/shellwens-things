001d89e3bb8bd39ac888dee64200a7c841b7af2a
ShellWen <me@shellwen.com>
2023-12-16 05:04:23 +0800

    :fire: remove Sentry
    Sentry doesn't support Cloudflare adapter

--- astro.config.js
+++ astro.config.js
@@ -3,7 +3,7 @@ import mdx from '@astrojs/mdx'
 import react from '@astrojs/react'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
-import sentry from '@sentry/astro'
+// import sentry from '@sentry/astro'
 import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
@@ -15,16 +15,17 @@ export default defineConfig({
     react(),
     sitemap(),
     tailwind(),
-    sentry({
-      dsn: 'https://7df36d7c0ed618e59ae41c6ded5b9a7c@o4506401530511360.ingest.sentry.io/4506401532018688',
-      release: `shellwens-things@${process.env.npm_package_version ?? 'unknown'}`,
-      // Sentry does not support source maps for Astro with Cloudflare adapter yet
-      // https://github.com/getsentry/sentry-javascript/issues/9777
-      // sourceMapsUploadOptions: {
-      //   project: 'shellwens-things',
-      //   authToken: process.env.SENTRY_AUTH_TOKEN,
-      // },
-    }),
+    // Sentry does not support Astro with Cloudflare adapter yet
+    // https://github.com/getsentry/sentry-javascript/issues/9777
+
+    // sentry({
+    //   dsn: 'https://7df36d7c0ed618e59ae41c6ded5b9a7c@o4506401530511360.ingest.sentry.io/4506401532018688',
+    //   release: `shellwens-things@${process.env.npm_package_version ?? 'unknown'}`,
+    // sourceMapsUploadOptions: {
+    //   project: 'shellwens-things',
+    //   authToken: process.env.SENTRY_AUTH_TOKEN,
+    // },
+    // }),
   ],
   adapter: cloudflare({
     imageService: 'cloudflare',
