76342f44804fa380851b81fd17fc1344ae22f281
ShellWen <me@shellwen.com>
2023-12-25 19:14:15 +0800

    :sparkles: add favicon, make site static only

--- astro.config.js
+++ astro.config.js
@@ -1,15 +1,12 @@
-import cloudflare from '@astrojs/cloudflare'
 import mdx from '@astrojs/mdx'
 import react from '@astrojs/react'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
-// import sentry from '@sentry/astro'
 import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://shellwen.com',
-  output: 'hybrid',
   integrations: [
     mdx(),
     react(),
@@ -27,9 +24,4 @@ export default defineConfig({
     // },
     // }),
   ],
-  adapter: cloudflare({
-    // Cloudflare's image service need to pay for extra
-    // imageService: 'cloudflare',
-    imageService: 'passthrough',
-  }),
 })
