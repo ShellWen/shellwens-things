513a1ca3224b49b7a879504853957c676ec413a8
ShellWen <me@shellwen.com>
2024-05-27 19:21:56 +0800

    :bug: bypass Squoosh detect of Astro

--- astro.config.js
+++ astro.config.js
@@ -16,7 +16,7 @@ imageService.entrypoint = './squooshService.js'
 // https://astro.build/config
 export default defineConfig({
   site: 'https://shellwen.com',
-  output: 'hybrid',
+  output: 'server',
   adapter: cloudflare({
     imageService: 'custom',
     platformProxy: {
