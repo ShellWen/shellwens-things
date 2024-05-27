5209523e32b45bc154f723b188a5c575cdcf2871
ShellWen <me@shellwen.com>
2024-05-27 19:49:50 +0800

    :zap: use Squoosh as image service

--- astro.config.js
+++ astro.config.js
@@ -4,23 +4,29 @@ import mdx from '@astrojs/mdx'
 import react from '@astrojs/react'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
-import { defineConfig } from 'astro/config'
+import { defineConfig, squooshImageService } from 'astro/config'
 import rehypeAutolinkHeadings from 'rehype-autolink-headings'
 import rehypeSlug from 'rehype-slug'
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { visualizer } from 'rollup-plugin-visualizer'
+
+const imageService = squooshImageService()
+imageService.entrypoint = './squooshService.js'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://shellwen.com',
   output: 'hybrid',
   adapter: cloudflare({
-    imageService: 'compile',
+    imageService: 'custom',
     platformProxy: {
       enabled: true,
       configPath: 'wrangler.toml',
     },
   }),
+  image: {
+    service: imageService,
+  },
   integrations: [
     mdx(),
     react(),
---
+++ squooshService.js
@@ -0,0 +1,3 @@
+import originalService from 'astro/assets/services/squoosh'
+
+export default originalService
