8f963ec35095797829aaa75d5485f91e7a058c63
ShellWen <me@shellwen.com>
2026-01-24 18:12:18 +0800

    :bug: fix reading time i18n

--- remark-reading-time.js
+++ remark-reading-time.js
@@ -1,12 +1,19 @@
 import { toString } from 'mdast-util-to-string'
 import { readingTime } from 'reading-time-estimator'
+import { zhCn } from 'reading-time-estimator/i18n/zh-cn'
 
 /** @import { RemarkPlugin } from '@astrojs/markdown-remark' */
 /** @type {RemarkPlugin} */
 export const remarkReadingTime = () => {
   return (tree, { data }) => {
     const textOnPage = toString(tree)
-    const t = readingTime(textOnPage, 300, 'cn')
+    const t = readingTime(textOnPage, {
+      wordsPerMinute: 300,
+      language: 'zh-cn',
+      translations: {
+        'zh-cn': zhCn,
+      },
+    })
     data.astro.frontmatter.minutesRead = '预计 ' + t.text
   }
 }
